39066df7b8f70e5872e41c468e7789b380254d63
awaismalik <awais.malik.q@gmail.com>
2021-12-17 14:47:41 +0500

    Landing page frontend completed

--- src/app/public/Pages/landing/landing.module.ts
+++ src/app/public/Pages/landing/landing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingComponent } from './landing.component';
+import { HeaderModule } from '../common/header/header.module';
+import { FooterModule } from '../common/footer/footer.module';
 
 @NgModule({
   declarations: [LandingComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderModule, FooterModule],
   exports: [LandingComponent],
 })
 export class LandingModule {}
