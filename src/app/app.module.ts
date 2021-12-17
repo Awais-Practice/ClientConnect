68debe59f20dd946da4b1cdab2c8a62dbe71d7dc
awaismalik <awais.malik.q@gmail.com>
2021-12-17 14:50:59 +0500

    Delete extra files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { LogInModule } from './log-in/log-in.module';
 import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -13,7 +12,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     MatIconModule,
-    LogInModule,
     BrowserAnimationsModule,
   ],
   providers: [],
